1ed0657e715bc9e268d8fd19fe9ddcce303ae281
asdfas <sadfakjsj@sina.com>
2015-11-11 17:59:47 +0800

    one

--- BuyBooks/Views/RootViewController.m
+++ BuyBooks/Views/RootViewController.m
@@ -31,6 +31,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.view.backgroundColor = [UIColor blueColor];
 //    隐藏上导航
     self.navigationController.navigationBar.hidden = YES;
     
